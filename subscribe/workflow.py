8a163ac3993a5a141c74418ea7dffa2748dd53cb
yinming <yinming@jingheoffice.com>
2024-12-10 10:53:07 +0800

    允许跳过证书校验

--- subscribe/workflow.py
+++ subscribe/workflow.py
@@ -65,7 +65,7 @@ class TaskConfig:
     liveness: bool = True
 
     # 是否强制开启 tls 及阻止跳过证书验证
-    disable_insecure: bool = True
+    disable_insecure: bool = False
 
     # 覆盖subconverter默认exclude规则
     ignorede: bool = False
